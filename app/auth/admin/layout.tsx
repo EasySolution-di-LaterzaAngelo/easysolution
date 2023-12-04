cdbb031efeab28976f8217a6a675a10d37a28563
MatteoDigiorgio <matthew.digiorgio@gmail.com>
2023-12-04 14:46:08 +0100

    Improved SEO

--- app/auth/admin/layout.tsx
+++ app/auth/admin/layout.tsx
@@ -10,6 +10,18 @@ export default function RootLayout({
 }) {
   return (
     <html>
+      <head>
+        <title>Easy Solution Di Laterza Angelo</title>
+        <meta
+          name='description'
+          content='Esplorate la nostra cartoleria di oggetti artigianali unici. Bomboniere, incisioni su legno e metallo e molto altro, tutti creati a mano con passione.'
+        />
+        <link rel='canonical' href='https://easysolution.vercel.app/' />
+        {/* IE */}
+        <link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
+        {/* other browsers */}
+        <link rel='icon' type='image/x-icon' href='/favicon.ico' />
+      </head>
       <body className={`flex flex-col bg-slate-50 ${styles.main}`}>
         <ProvidersWrapper>
           <SubHeader />
